9fd73b3636a2ff37ecdf490a3f56270e6e86a373
Jake Eissler <jakeeissler1@gmail.com>
2022-06-30 11:25:49 -0600

    floating navbar complete

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "./nav.css";
+import { BiHome } from "react-icons/bi";
+import { BiUser } from "react-icons/bi";
+import { HiOutlineClipboardList } from "react-icons/hi";
+import { AiOutlineCheckSquare } from "react-icons/ai";
+import { AiOutlinePhone } from "react-icons/ai";
+import { useState } from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <BiHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <BiUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <HiOutlineClipboardList />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <AiOutlineCheckSquare />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <AiOutlinePhone />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
