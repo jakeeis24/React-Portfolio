7bf32edc98ecbfaeae4270b1a051dcac3ccb3509
Jake Eissler <jakeeissler1@gmail.com>
2022-06-30 12:33:03 -0600

    experience component finished, still need to add CSS

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,56 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaHammer } from "react-icons/fa";
+import { FiThumbsUp } from "react-icons/fi";
+import { AiOutlineFolderOpen } from "react-icons/ai";
+
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaHammer className="about__icon" />
+              <h5>Experience</h5>
+              <small>3 month Bootcamp Feb-May 2022</small>
+            </article>
+            <article className="about__card">
+              <FiThumbsUp className="about__icon" />
+              <h5>Open For Work</h5>
+              <small>I want to start my career in web development!</small>
+            </article>
+            <article className="about__card">
+              <AiOutlineFolderOpen className="about__icon" />
+              <h5>Projects</h5>
+              <small>What should I put here??</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+            accusamus illum doloremque laudantium esse eius repudiandae velit
+            voluptate nihil minima sapiente ullam quas, quia commodi. Atque
+            harum illo dicta modi!
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,78 @@
 import React from "react";
 import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>Skills I have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Bootstrap</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Javascript</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>React</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+          </div>
+        </div>
+        {/* end of front end */}
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Node JS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>MongoDB</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>MySQL</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>GraphQL</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Express JS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
