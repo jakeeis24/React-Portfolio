ebb0070fa01b1467b772345019d3b2c8bd2a11a2
Jake Eissler <jakeeissler1@gmail.com>
2022-07-02 09:23:43 -0600

    testimonials component complete utilizing swiperjs. I may change this component since I do not have any testimonials

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,13 +6,18 @@ import { BsTwitter } from "react-icons/bs";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/jake-eissler-3aaa6b190
+"
+        target="_blank"
+        rel="noreferrer"
+      >
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank">
+      <a href="https://github.com/jakeeis24" target="_blank" rel="noreferrer">
         <BsGithub />
       </a>
-      <a href="https://twitter.com" target="_blank">
+      <a href="https://twitter.com/jakeeis24" target="_blank" rel="noreferrer">
         <BsTwitter />
       </a>
     </div>
