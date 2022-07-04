e8dcc9b187622659f952a7a23e0e9470a315c3b3
Jake Eissler <jakeeissler1@gmail.com>
2022-07-04 09:44:00 -0600

    form added to contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./contact.css";
 import { FiMail } from "react-icons/fi";
 import { RiMessengerLine } from "react-icons/ri";
+import { FiTwitter } from "react-icons/fi";
 
 const Contact = () => {
   return (
@@ -25,7 +26,7 @@ const Contact = () => {
           </article>
           <article className="contact__option">
             <RiMessengerLine />
-            <h4>Facebook Messenger</h4>
+            <h4>Messenger</h4>
             <h5>Jake Eissler</h5>
             <a
               href="https://m.me/jake.eissler"
@@ -36,7 +37,7 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact__option">
-            <FiMail />
+            <FiTwitter />
             <h4>Twitter</h4>
             <h5>jakeeis24</h5>
             <a
@@ -44,12 +45,29 @@ const Contact = () => {
               target="_blank"
               rel="noreferrer"
             >
-              Send a DM
+              Send a Message
             </a>
           </article>
         </div>
         {/* End of Contact Options */}
-        <form action=""></form>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
