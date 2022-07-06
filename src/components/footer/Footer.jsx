c18266192881c628d5ef569fc0cdb41772f37488
Jake Eissler <jakeeissler1@gmail.com>
2022-07-06 10:12:44 -0600

    footer component created and styled. Mobile responsive.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./footer.css";
+import { FiFacebook } from "react-icons/fi";
+import { GrInstagram } from "react-icons/gr";
+import { FiTwitter } from "react-icons/fi";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        JAKELINK
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FiFacebook />
+        </a>
+        <a href="https://instagram.com">
+          <GrInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <FiTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; EisslerWebDev. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
